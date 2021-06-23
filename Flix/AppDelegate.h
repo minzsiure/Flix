0e07601c375625164165d891e804d68d9af33c54
Eva Xie <xieyi@fb.com>
2021-06-23 11:52:41 -0400

    Initial Commit

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by Eva Xie on 6/23/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Flix/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Flix
+//
+//  Created by Eva Xie on 6/23/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
